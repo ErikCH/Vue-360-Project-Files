4857f43b9b3aecf701f80f193dd514f204a489ab
Erik Hanchett <erik.hanchett@gmail.com>
2020-02-28 14:18:14 -0800

    first commit

---
+++ week4/DeployApp/deployapp/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h3>Hello World From Vue</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
